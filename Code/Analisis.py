6867fbaa223faa7abd61b893ded753e12deb17bc
juankaUAB <57271164+juankaUAB@users.noreply.github.com>
2021-11-16 16:07:02 +0100

    apartat B done

--- Code/Analisis.py
+++ Code/Analisis.py
@@ -5,6 +5,9 @@ import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 from sklearn import svm, datasets
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import f1_score, precision_recall_curve, average_precision_score, roc_curve, auc
+
 
 dataset = pd.read_csv("../BD/price_classification.csv")
 dataset_values = dataset.values
@@ -18,7 +21,7 @@ n_clases = dataset["price_range"].nunique()
 plt.figure()
 sns.set()
 for i in range(dataset.shape[1] - 1):
-    sns.scatterplot(data=dataset, x=titles[i], y=titles[-1], palette='pastel')
+    sns.scatterplot(data=dataset, x=titles[i], y=titles[-1], palette='pastel', hue="price_range")
     plt.savefig("../Graficas/disp/Caracteristica" + str(i+1) + ".png")
     plt.clf()
     
@@ -42,3 +45,140 @@ sns.heatmap(dataset.corr(), ax=ax, cmap=cmap, vmin=0, vmax=1, center=0,
             square=True, linewidths=.5, cbar_kws={"shrink": .5}, annot=True)
 plt.savefig("../Graficas/heatmap/mapa-de-calor.png")
 plt.clf()
+
+X = dataset_values[:,:-1]
+
+scaler = StandardScaler()
+scaler.fit(X)
+X = scaler.transform(X)
+
+y = dataset_values[:,-1]
+particions = [0.5, 0.8, 0.7]
+
+for part in particions:
+    x_t, x_v, y_t, y_v = train_test_split(X, y, train_size=part)
+    
+    #Creem el regresor logístic
+    logireg = LogisticRegression(C=2.0, fit_intercept=True, penalty='l2', tol=0.001)
+
+    # l'entrenem
+    logireg.fit(x_t, y_t)
+
+    print ("Correct classification Logistic ", part, "% of the data: ", logireg.score(x_v, y_v))
+    
+    #Creem el regresor logístic
+    svc = svm.SVC(C=10.0, kernel='linear', gamma=0.6, probability=True)
+
+    # l'entrenem 
+    svc.fit(x_t, y_t)
+    probs = svc.predict_proba(x_v)
+    print ("Correct classification SVM      ", part, "% of the data: ", svc.score(x_v, y_v))
+
+
+# Compute Precision-Recall and plot curve
+precision = {}
+recall = {}
+average_precision = {}
+plt.figure()
+for i in range(n_clases):
+    precision[i], recall[i], _ = precision_recall_curve(y_v == i, probs[:, i])
+    average_precision[i] = average_precision_score(y_v == i, probs[:, i])
+
+    plt.plot(recall[i], precision[i],
+    label='Precision-recall curve of class {0} (area = {1:0.2f})'
+                           ''.format(i, average_precision[i]))
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.legend(loc="upper right")
+
+    
+# Compute ROC curve and ROC area for each class
+fpr = {}
+tpr = {}
+roc_auc = {}
+for i in range(n_clases):
+    fpr[i], tpr[i], _ = roc_curve(y_v == i, probs[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+# Compute micro-average ROC curve and ROC area
+# Plot ROC curve
+plt.figure()
+for i in range(n_clases):
+    plt.plot(fpr[i], tpr[i], label='ROC curve of class {0} (area = {1:0.2f})' ''.format(i, roc_auc[i]))
+plt.legend()
+
+def make_meshgrid(x, y, h=.02):
+    """Create a mesh of points to plot in
+
+    Parameters
+    ----------
+    x: data to base x-axis meshgrid on
+    y: data to base y-axis meshgrid on
+    h: stepsize for meshgrid, optional
+
+    Returns
+    -------
+    xx, yy : ndarray
+    """
+    x_min, x_max = x.min() - 1, x.max() + 1
+    y_min, y_max = y.min() - 1, y.max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    return xx, yy
+
+def plot_contours(ax, clf, xx, yy, **params):
+    """Plot the decision boundaries for a classifier.
+
+    Parameters
+    ----------
+    ax: matplotlib axes object
+    clf: a classifier
+    xx: meshgrid ndarray
+    yy: meshgrid ndarray
+    params: dictionary of params to pass to contourf, optional
+    """
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    out = ax.contourf(xx, yy, Z, **params)
+    return out
+
+def show_C_effect(X, y, C=1.0, gamma=0.7, degree=3):
+
+
+    # we create an instance of SVM and fit out data. We do not scale our
+    # data since we want to plot the support vectors
+    # title for the plots
+    titles = ('SVC with linear kernel',
+              'LinearSVC (linear kernel)',
+              'SVC with RBF kernel',
+              'SVC with polynomial (degree 3) kernel')
+
+    #C = 1.0  # SVM regularization parameter
+    models = (svm.SVC(kernel='linear', C=C),
+              svm.LinearSVC(C=C, max_iter=1000000),
+              svm.SVC(kernel='rbf', gamma=gamma, C=C),
+              svm.SVC(kernel='poly', degree=degree, gamma='auto', C=C))
+    models = (clf.fit(X, y) for clf in models)
+
+    plt.close('all')
+    fig, sub = plt.subplots(2, 2, figsize=(14,9))
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+
+    X0, X1 = X[:, 0], X[:, 1]
+    xx, yy = make_meshgrid(X0, X1)
+
+    for clf, title, ax in zip(models, titles, sub.flatten()):
+        plot_contours(ax, clf, xx, yy,
+                      cmap=plt.cm.coolwarm, alpha=0.8)
+        ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
+        ax.set_xlim(xx.min(), xx.max())
+        ax.set_ylim(yy.min(), yy.max())
+        ax.set_xlabel('px_width')
+        ax.set_ylabel('ram')
+        ax.set_xticks(())
+        ax.set_yticks(())
+        ax.set_title(title)
+
+    plt.savefig("../Graficas/c-effect.png")
+    
+show_C_effect(X[:,12:14], y, C=0.1)
