098accdec698687bd4cc4fe7b8de5919a798c902
juankaUAB <57271164+juankaUAB@users.noreply.github.com>
2021-11-17 20:41:16 +0100

    busqueda exhaustiva de hiperparametros + organizacion repo

--- Code/ApartatA.py
+++ Code/ApartatA.py
@@ -15,8 +15,37 @@ from sklearn.metrics import accuracy_score,  precision_score, recall_score, roc_
 from sklearn.metrics import confusion_matrix, plot_confusion_matrix, precision_recall_curve
 from sklearn.linear_model import Perceptron
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+''' DICCIONARIO PARA LA BUSQUEDA EXHAUSTIVA DE HYPERPARAMETERS'''
+param_svm = {'C': [0.1,1, 10, 100], 'gamma': [1,0.1,0.01,0.001],'kernel': ['rbf', 'poly', 'sigmoid']}
+param_perceptron = {'penalty': ['l2','l1','elasticnet'], 'alpha': [0.0001, 0.001, 0.01, 0.1, 1],
+                    'fit_intercept': [True, False], 'shuffle': [True, False]}
+param_knn = { 'n_neighbors' : [5,7,9,11,13,15],
+               'weights' : ['uniform','distance'],
+               'metric' : ['minkowski','euclidean','manhattan']}
+param_decisiontree = {
+    'max_depth': [2, 3, 5, 10, 20],
+    'min_samples_leaf': [5, 10, 20, 50, 100],
+    'criterion': ["gini", "entropy"]
+}
+param_randomforest = {
+    'bootstrap': [True],
+    'max_depth': [80, 90, 100, 110],
+    'max_features': [2, 3],
+    'min_samples_leaf': [3, 4, 5],
+    'min_samples_split': [8, 10, 12],
+    'n_estimators': [100, 200, 300, 1000]
+
+}
+param_logireg = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000] }
 
 
+
+param_grid = [param_svm, param_perceptron, param_knn, param_decisiontree, param_randomforest, param_logireg]
+
+
+'''crear base de datos'''
 dataset = pd.read_csv("../BD/price_classification.csv")
 dataset_values = dataset.values
 
@@ -31,8 +60,10 @@ X = scaler.transform(X)
 
 y = dataset_values[:,-1]
 particions = [0.5, 0.8, 0.7]
-models = [svm.SVC(probability=True)]
-nom_models = ["Support Vector Machines"]
+models = [svm.SVC(probability=True), Perceptron(), KNeighborsClassifier(), DecisionTreeClassifier(), RandomForestClassifier(), LogisticRegression()]
+nom_models = ["Support Vector Machines", "Perceptron", "KNN", "Decision Tree", "Random Forest", "Logistic Regression"]
+
+
 
 for i,model in enumerate(models):
     print("---- ", nom_models[i], " ----")
@@ -99,6 +130,14 @@ for i,model in enumerate(models):
             plt.plot(fpr[j], tpr[j], label='ROC curve of class {0} (area = {1:0.2f})' ''.format(j, roc_auc[j]))
         plt.legend()
         plt.savefig("../Graficas-A/roc/curva-roc" + str(nom_models[i]) + ".png")
+    '''Busqueda exhaustiva de los mejores parametros'''
+    print("BUSQUEDA EXHAUSTIVA DE PARAMETROS")
+    grid = GridSearchCV(model, param_grid[i], verbose=3, n_jobs=-1)
+    grid.fit(X,y)
+    print("Els millors parametres: ",grid.best_params_)
+    print("El millor score: ", grid.best_score_)
+    print("")
+    
     
 
 
